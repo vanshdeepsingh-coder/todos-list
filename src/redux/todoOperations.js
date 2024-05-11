fa236ab0f5eea2d016c3d8e5f351320c0763f760
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-05-12 00:22:46 +0530

    removing localstorage dependency

--- src/redux/todoOperations.js
+++ src/redux/todoOperations.js
@@ -1,36 +1,5 @@
 let initialState=[];
 
-function convertToString(todo){
-    return JSON.stringify(todo)
-}
-
-function convertToJSON(todo){
-    return JSON.parse(todo)
-}
-
-function setTodoInLocalStorage(todos){
-    localStorage.setItem("todoData",convertToString(todos))
-}
-
-function getTodoFromLocalStorage(){
-    const todos=localStorage.getItem("todoData")
-    return todos
-}
-
-async function fetchTodoData(){
-    const response= await fetch('http://localhost:3001/todos');
-    const result=await response.json();
-    setTodoInLocalStorage(result)
-}
-
-fetchTodoData()
-
-const todos=getTodoFromLocalStorage()
-
-if(todos){
-    initialState=convertToJSON(todos)
-}
-
 export default function todoOperations(state=initialState,action){
     switch(action.type){
         case 'INITIAL-STATE':
@@ -38,11 +7,9 @@ export default function todoOperations(state=initialState,action){
         case 'ADD-TODO':
             const newPayload={...action.payload}
             const addedData=[...state,newPayload];
-            setTodoInLocalStorage(addedData)
             return addedData
         case 'DELETE-TODO':
             const deletedData=state.filter((todos)=>todos.id!=action.payload)
-            setTodoInLocalStorage(deletedData)
             return deletedData;
         case 'EDIT-TODO':
             const newTodoList=[...state]
@@ -53,7 +20,6 @@ export default function todoOperations(state=initialState,action){
                     data.task=action.payload.task
                 }
             })
-            setTodoInLocalStorage(newTodoList)
             return newTodoList
         default:
             return state
